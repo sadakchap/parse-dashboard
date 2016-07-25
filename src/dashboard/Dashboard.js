8fd552155db19854ead5fc0a43e2fa57699120c9
Seiji Akiyama <seiji.akiyama2.0@gmail.com>
2016-07-25 18:53:41 -0300

    Ok, let's rollback

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -5,7 +5,7 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import AccountManager     from 'lib/AccountManager'; // user workaround
+//import AccountManager     from 'lib/AccountManager'; // user workaround
 import AccountOverview    from './Account/AccountOverview.react';
 import AccountView        from './AccountView.react';
 import AnalyticsOverview  from './Analytics/Overview/Overview.react';
@@ -119,8 +119,8 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    get('/parse-dashboard-config.json').then(({ apps, newFeaturesInLatestVersion = [], user }) => {
-      AccountManager.setCurrentUser(user);
+    get('/parse-dashboard-config.json').then(({ apps, newFeaturesInLatestVersion = [] }) => {
+      //AccountManager.setCurrentUser(user);
       this.setState({ newFeaturesInLatestVersion });
       let appInfoPromises = apps.map(app => {
         if (app.serverURL.startsWith('https://api.parse.com/1')) {
