717307cb1440441a6ce883f789e1a26edac4f160
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2021-05-13 16:33:37 -0700

    Pass credentials to /me/hub

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -83,7 +83,7 @@ const AccountSettingsPage = () => (
 
 async function fetchHubUser() {
   try {
-    return (await axios.get(`${b4aSettings.BACK4APP_API_PATH}/me/hub`)).data;
+    return (await axios.get(`${b4aSettings.BACK4APP_API_PATH}/me/hub`, { withCredentials: true })).data;
   } catch (err) {
     throw err.response && err.response.data && err.response.data.error ? err.response.data.error : err
   }
