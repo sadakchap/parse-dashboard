e17d584b50b19276d4279d4f586e0faa7a5f2e79
sadakchap <aliceprerna@gmail.com>
2021-06-10 12:06:27 +0530

    added a loading screen in logs

--- src/dashboard/Data/Logs/Logs.react.js
+++ src/dashboard/Data/Logs/Logs.react.js
@@ -5,17 +5,18 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import CategoryList  from 'components/CategoryList/CategoryList.react';
-import DashboardView from 'dashboard/DashboardView.react';
-import EmptyState    from 'components/EmptyState/EmptyState.react';
-import LogView       from 'components/LogView/LogView.react';
-import LogViewEntry  from 'components/LogView/LogViewEntry.react';
-import React         from 'react';
-import ReleaseInfo   from 'components/ReleaseInfo/ReleaseInfo';
-import Toolbar       from 'components/Toolbar/Toolbar.react';
-import Icon          from 'components/Icon/Icon.react';
+import CategoryList    from 'components/CategoryList/CategoryList.react';
+import DashboardView   from 'dashboard/DashboardView.react';
+import EmptyState      from 'components/EmptyState/EmptyState.react';
+import LogView         from 'components/LogView/LogView.react';
+import LogViewEntry    from 'components/LogView/LogViewEntry.react';
+import React           from 'react';
+import ReleaseInfo     from 'components/ReleaseInfo/ReleaseInfo';
+import Toolbar         from 'components/Toolbar/Toolbar.react';
+import LoaderContainer from 'components/LoaderContainer/LoaderContainer.react';
+import Icon            from 'components/Icon/Icon.react';
 
-import styles        from 'dashboard/Data/Logs/Logs.scss';
+import styles          from 'dashboard/Data/Logs/Logs.scss';
 
 let subsections = {
   access: 'Access',
@@ -31,7 +32,8 @@ export default class Logs extends DashboardView {
     this.subsection = 'Logs';
 
     this.state = {
-      logs: undefined,
+      loading: false,
+      logs: [],
       release: undefined
     };
 
@@ -51,23 +53,27 @@ export default class Logs extends DashboardView {
   }
 
   fetchLogs(app, type) {
+    // set loading true
+    this.setState({
+      loading: true
+    });
     let serverLogType = ['access', 'system'];
     if (serverLogType.includes(type)) {
       app.fetchServerLogs().then(res => {
-        this.setState({ logs: [] });
         this.setState({
-          logs: type === 'access' ? res.access : res.docker
+          logs: type === 'access' ? res.access : res.docker,
+          loading: false
         });
-      }, err => this.setState({ logs: [] }));
+      }, err => this.setState({ logs: [], loading: false }));
       return;
     }
+    
     let typeParam = (type || 'INFO').toUpperCase();
     app.getLogs(typeParam).then(
       (logs) => {
-        this.setState({logs:[]})
-        this.setState({ logs })
+        this.setState({ logs, loading: false });
       },
-      () => this.setState({ logs: [] })
+      () => this.setState({ logs: [], loading: false })
     );
   }
 
@@ -120,33 +126,31 @@ export default class Logs extends DashboardView {
       );
     }
     let content = null;
-    if (!Array.isArray(this.state.logs)) {
-      // loading
-    } else if (this.state.logs.length === 0) {
-      content = (
-        <div className={styles.empty}>
-          <EmptyState
-            icon='files-outline'
-            title='No logs in the last 30 days'
-            description='When you start using Cloud Code, your logs will show up here.'
-            cta='Learn more'
-            action={() => window.location = 'http://docs.parseplatform.org/cloudcode/guide'} />
-        </div>
-      );
-    } else {
-      content = (
-        <div className={styles.content}>
-          <LogView>
-            {this.state.logs.map(({ message, timestamp }) => <LogViewEntry
-              key={timestamp}
-              text={message}
-              timestamp={timestamp} />)}
-          </LogView>
-        </div>
-      );
-    }
+    content = (
+      <LoaderContainer loading={this.state.loading} solid={false}>
+        {this.state.logs.length === 0 && !this.state.loading ? (
+          <div className={styles.content}>
+            <EmptyState
+              icon='files-outline'
+              title='No logs in the last 30 days'
+              description='When you start using Cloud Code, your logs will show up here.'
+              cta='Learn more'
+              action={() => window.location = 'http://docs.parseplatform.org/cloudcode/guide'} />
+          </div>
+        ) : (
+          <div className={styles.content}>
+            <LogView>
+              {this.state.logs.map(({ message, timestamp }) => <LogViewEntry
+                key={timestamp}
+                text={message}
+                timestamp={timestamp} />)}
+            </LogView>
+          </div>
+        )}
+      </LoaderContainer>
+    );
     return (
-      <div className={styles.logs}>
+      <div>
         {content}
         {toolbar}
       </div>
