40ca05807ad4c615245504dde1b5043b90da9b6a
sadakchap <aliceprerna@gmail.com>
2021-07-21 22:46:54 +0530

    showing error popup on Index creation

--- src/dashboard/IndexManager/IndexForm.react.js
+++ src/dashboard/IndexManager/IndexForm.react.js
@@ -193,9 +193,10 @@ class IndexForm extends Component {
       title: 'Are you sure you want to create the indexes?',
       text: 'This process will run in background and could take minutes, depending on your class size.',
       type: 'warning',
+      showLoaderOnConfirm: true,
       confirmButtonText: 'Confirm',
       preConfirm: () => {
-        this.props.onConfirm(indexConfiguration)
+        return this.props.onConfirm(indexConfiguration)
       },
       showCancelButton: true
     }])
