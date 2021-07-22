d361fcc4dce43169c19d9a7231301b984c487223
Prerna Mehra <aliceprerna@gmail.com>
2021-07-23 00:28:27 +0530

    showing error popup on Index creation (#320)

--- src/dashboard/IndexManager/IndexManager.react.js
+++ src/dashboard/IndexManager/IndexManager.react.js
@@ -212,7 +212,7 @@ class IndexManager extends DashboardView {
       errorMessages.push('Only one text index is allowed per class')
     }
     if (errorMessages.length) {
-      Swal.fire({
+      Swal.insertQueueStep({
         title: 'We found some errors',
         html: `<p style="text-align: center">${errorMessages.join('</p><p style="text-align: center">')}</p>`,
         type: 'error',
@@ -220,11 +220,11 @@ class IndexManager extends DashboardView {
       })
     } else {
       const { className } = this.props.params
-      this.context.currentApp.createIndex(className, indexConfiguration)
+      return this.context.currentApp.createIndex(className, indexConfiguration)
         .then(() => {
           this.closeIndexForm()
         }).catch(e => {
-          Swal.fire({
+          Swal.insertQueueStep({
             title: 'Index creation failure',
             text: 'Error while creating the indexes. Please try again later.',
             type: 'error'
