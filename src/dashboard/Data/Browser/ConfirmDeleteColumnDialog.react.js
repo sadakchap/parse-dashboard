ccf6eb929c5e8daa993e3a262104fed8389cb7b1
sadakchap <aliceprerna@gmail.com>
2021-08-20 11:36:04 +0530

    change remove column modal title text

--- src/dashboard/Data/Browser/ConfirmDeleteColumnDialog.react.js
+++ src/dashboard/Data/Browser/ConfirmDeleteColumnDialog.react.js
@@ -5,11 +5,8 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import Field     from 'components/Field/Field.react';
-import Label     from 'components/Label/Label.react';
 import Modal     from 'components/Modal/Modal.react';
 import React     from 'react';
-import TextInput from 'components/TextInput/TextInput.react';
 
 export default class ConfirmDeleteColumnDialog extends React.Component {
 
@@ -20,9 +17,9 @@ export default class ConfirmDeleteColumnDialog extends React.Component {
       <Modal
         type={Modal.Types.DANGER}
         icon='warn-outline'
-        title={`Delete '${this.props.field}' column?`}
-        subtitle={'This action cannot be undone!'}
-        confirmText={`Yes, delete`}
+        title='Delete this column?'
+        subtitle='This action cannot be undone!'
+        confirmText='Yes, delete'
         cancelText={'Never mind, don\u2019t.'}
         onCancel={this.props.onCancel}
         onConfirm={this.props.onConfirm}>
