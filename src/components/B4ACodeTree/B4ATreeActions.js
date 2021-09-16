9566c93e798961f00ca1b76f8e72372042f1ba2e
Prerna Mehra <aliceprerna@gmail.com>
2021-09-16 23:11:42 +0530

    changed context menu create label for folder type (#407)

--- src/components/B4ACodeTree/B4ATreeActions.js
+++ src/components/B4ACodeTree/B4ATreeActions.js
@@ -154,6 +154,9 @@ const addFilesOnTree = async (files, currentCode, selectedFolder) => {
 // Configure the menu that is shown on right-click based on files type
 const customMenu = node => {
   let items = $.jstree.defaults.contextmenu.items();
+  if (node.type === 'folder') {
+    items.create.label = 'Create Folder';
+  }
   items.create.action = function (data) {
     create(data.reference)
   };
