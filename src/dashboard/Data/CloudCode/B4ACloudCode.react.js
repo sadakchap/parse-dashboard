a143c5b1cefd384cadb8dfcadc6cdde0b3bea2f9
Prerna Mehra <aliceprerna@gmail.com>
2021-10-12 11:13:45 +0530

    fix reference error in B4ACloudCode.react.js file (#434)

--- src/dashboard/Data/CloudCode/B4ACloudCode.react.js
+++ src/dashboard/Data/CloudCode/B4ACloudCode.react.js
@@ -5,20 +5,25 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import React           from 'react';
-import { withRouter, Prompt }  from 'react-router';
-import history         from 'dashboard/history';
-import $               from 'jquery';
-import axios           from 'axios';
-import B4AAlert        from 'components/B4AAlert/B4AAlert.react';
-import Button          from 'components/Button/Button.react';
-import B4ACodeTree     from 'components/B4ACodeTree/B4ACodeTree.react';
+import React                  from 'react';
+import { withRouter, Prompt } from 'react-router';
+import history                from 'dashboard/history';
+import $                      from 'jquery';
+import axios                  from 'axios';
+import B4AAlert               from 'components/B4AAlert/B4AAlert.react';
+import Button                 from 'components/Button/Button.react';
+import B4ACodeTree            from 'components/B4ACodeTree/B4ACodeTree.react';
 import {
   getFiles,
   updateTreeContent
-}                      from 'components/B4ACodeTree/B4ATreeActions';
+}                             from 'components/B4ACodeTree/B4ATreeActions';
 import B4ACloudCodeToolbar    from 'dashboard/Data/CloudCode/B4ACloudCodeToolbar.react';
-import CloudCode       from 'dashboard/Data/CloudCode/CloudCode.react';
+import CloudCode              from 'dashboard/Data/CloudCode/CloudCode.react';
+import LoaderContainer        from 'components/LoaderContainer/LoaderContainer.react';
+import LoaderDots             from 'components/LoaderDots/LoaderDots.react';
+import Icon                   from 'components/Icon/Icon.react';
+import Modal                  from 'components/Modal/Modal.react';
+import styles                 from 'dashboard/Data/CloudCode/CloudCode.scss';
 
 class B4ACloudCode extends CloudCode {
   constructor() {
