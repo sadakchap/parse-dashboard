0f1c37a25a147d2bc8bf38cb756e9f079e1094a3
sadakchap <aliceprerna@gmail.com>
2021-11-25 11:52:19 +0530

    serving js worker script for react-ace editor lint

--- Parse-Dashboard/app.js
+++ Parse-Dashboard/app.js
@@ -209,6 +209,11 @@ module.exports = function(config, options) {
       `);
     });
 
+    // Server worker-javascript for React-Ace
+    app.get('/worker-javascript.js', function(req, res) {
+      res.sendFile(path.join(__dirname, 'worker-javascript.js'), err => console.log(err));
+    });
+
     // For every other request, go to index.html. Let client-side handle the rest.
     app.get('/*', function(req, res) {
       if (users && (!req.user || !req.user.isAuthenticated)) {
