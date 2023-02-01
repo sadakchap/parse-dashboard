917acd816944d517a0833d4755265e0ead1e1774
nataliaconde <nataliaconde>
2023-02-01 07:16:35 -0300

    add tooltip to appname

--- src/components/Sidebar/AppName.react.js
+++ src/components/Sidebar/AppName.react.js
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "components/Sidebar/Sidebar.scss";
+import Tooltip from 'components/Tooltip/SimpleTooltip.react';
 
 const AppName = ({ name, pin, onClick }) => (
   <div className={styles.currentApp}>
     <div className={styles.currentAppNameGroup} onClick={onClick}>
-      <div className={styles.currentAppName}>{name}</div>
+      <Tooltip 
+        className={styles.Tooltip}
+        value={(
+          <span>{name}</span>
+        )}
+      >
+        <div className={styles.currentAppName}>{name}</div>
+      </Tooltip>
       <div className={styles.appsSelectorArrow}></div>
     </div>
     {pin}
