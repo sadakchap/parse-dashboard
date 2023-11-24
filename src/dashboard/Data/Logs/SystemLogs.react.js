fe20583d8365267d2161de6a6c83b788933ae5ba
sadakchap <aliceprerna@gmail.com>
2023-11-24 11:57:53 +0530

    logs lint

--- src/dashboard/Data/Logs/SystemLogs.react.js
+++ src/dashboard/Data/Logs/SystemLogs.react.js
@@ -5,33 +5,33 @@
  * This source code is licensed under the license found in the LICENSE file in
  * the root directory of this source tree.
  */
-import B4AAlert from "components/B4AAlert/B4AAlert.react";
-import CategoryList from "components/CategoryList/CategoryList.react";
-import DashboardView from "dashboard/DashboardView.react";
-import EmptyState from "components/EmptyState/EmptyState.react";
-import React from "react";
-import ReleaseInfo from "components/ReleaseInfo/ReleaseInfo";
-import Toolbar from "components/Toolbar/Toolbar.react";
-import LoaderContainer from "components/LoaderContainer/LoaderContainer.react";
-import Icon from "components/Icon/Icon.react";
-import ServerLogsView from "components/ServerLogsView/ServerLogsView.react";
+import B4AAlert from 'components/B4AAlert/B4AAlert.react';
+import CategoryList from 'components/CategoryList/CategoryList.react';
+import DashboardView from 'dashboard/DashboardView.react';
+import EmptyState from 'components/EmptyState/EmptyState.react';
+import React from 'react';
+import ReleaseInfo from 'components/ReleaseInfo/ReleaseInfo';
+import Toolbar from 'components/Toolbar/Toolbar.react';
+import LoaderContainer from 'components/LoaderContainer/LoaderContainer.react';
+import Icon from 'components/Icon/Icon.react';
+import ServerLogsView from 'components/ServerLogsView/ServerLogsView.react';
 
-import styles from "dashboard/Data/Logs/Logs.scss";
+import styles from 'dashboard/Data/Logs/Logs.scss';
 
-let alertWhatIsMessage = (
+const alertWhatIsMessage = (
   <div>
-    <p style={{ height: "auto" }}>
+    <p style={{ height: 'auto' }}>
       In this section, you will find the messages related to general logs of
       your Parse Server application and all logging levels associated with
       either success or error of your Cloud Code Functions provided by options
-      like console.log() or console.error(). Check our{" "}
+      like console.log() or console.error(). Check our{' '}
       <a
         href="https://www.back4app.com/docs/platform/parse-server-logs"
         target="_blank"
         rel="noopener noreferrer"
       >
         doc
-      </a>{" "}
+      </a>{' '}
       to know more about the logs.
     </p>
   </div>
@@ -39,12 +39,12 @@ let alertWhatIsMessage = (
 export default class SystemLogs extends DashboardView {
   constructor() {
     super();
-    this.section = "Cloud Code";
-    this.subsection = "Logs";
+    this.section = 'Cloud Code';
+    this.subsection = 'Logs';
 
     this.state = {
       loading: false,
-      logs: "",
+      logs: '',
       release: undefined,
       showWhatIs: localStorage.getItem('showSystemLogsInfo') !== 'false'
     };
@@ -63,10 +63,10 @@ export default class SystemLogs extends DashboardView {
       // check if the changes are in currentApp serverInfo status
       // if not return without making any request
       if (this.props.apps !== nextProps.apps) {
-        let updatedCurrentApp = nextProps.apps.find(ap => ap.slug === this.props.match.params.appId);
-        let prevCurrentApp = this.props.apps.find(ap => ap.slug === this.props.match.params.appId);
+        const updatedCurrentApp = nextProps.apps.find(ap => ap.slug === this.props.match.params.appId);
+        const prevCurrentApp = this.props.apps.find(ap => ap.slug === this.props.match.params.appId);
         const shouldUpdate = updatedCurrentApp.serverInfo.status !== prevCurrentApp.serverInfo.status;
-        if (!shouldUpdate) return;
+        if (!shouldUpdate) {return;}
       }
       this.fetchLogs(nextContext.currentApp);
       // this.fetchRelease(nextContext.currentApp);
@@ -107,17 +107,17 @@ export default class SystemLogs extends DashboardView {
   */
 
   renderSidebar() {
-    let { path } = this.props.match;
-    const current = path.substr(path.lastIndexOf("/") + 1, path.length - 1);
+    const { path } = this.props.match;
+    const current = path.substr(path.lastIndexOf('/') + 1, path.length - 1);
     return (
       <CategoryList
         current={current}
-        linkPrefix={"logs/"}
+        linkPrefix={'logs/'}
         categories={[
-          { name: "System", id: "system" },
-          { name: "Info", id: "info" },
-          { name: "Error", id: "error" },
-          { name: "Access", id: "access" }
+          { name: 'System', id: 'system' },
+          { name: 'Info', id: 'info' },
+          { name: 'Error', id: 'error' },
+          { name: 'Access', id: 'access' }
         ]}
       />
     );
@@ -154,7 +154,7 @@ export default class SystemLogs extends DashboardView {
     );
     let content = null;
     let alertWhatIs = null;
-    
+
     alertWhatIs = <B4AAlert
       show={this.state.showWhatIs}
       handlerCloseEvent={this.handlerCloseAlert}
@@ -164,16 +164,16 @@ export default class SystemLogs extends DashboardView {
     content = (
       <LoaderContainer loading={this.state.loading} solid={false}>
         <div className={styles.content}>
-          {!this.state.loading && this.state.logs === "" && (
-              <EmptyState
-                icon="files-outline"
-                title="No System logs in the last 30 days"
-                description={`In this section, you will find the messages related to general logs of your Parse Server application and all logging levels associated with either success or error of your Cloud Code Functions provided by options like console.log() or console.error()`}
-                cta="Learn more"
-                action={"https://www.back4app.com/docs/platform/parse-server-logs"}
-              />
-          )} 
-          {!this.state.loading && this.state.logs !== "" && (
+          {!this.state.loading && this.state.logs === '' && (
+            <EmptyState
+              icon="files-outline"
+              title="No System logs in the last 30 days"
+              description={'In this section, you will find the messages related to general logs of your Parse Server application and all logging levels associated with either success or error of your Cloud Code Functions provided by options like console.log() or console.error()'}
+              cta="Learn more"
+              action={'https://www.back4app.com/docs/platform/parse-server-logs'}
+            />
+          )}
+          {!this.state.loading && this.state.logs !== '' && (
             <div>
               {alertWhatIs}
               <ServerLogsView type="system" logs={this.state.logs} />
