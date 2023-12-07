7ecb3cb6c26eb6f08b53867a6800def8f6e1a13f
sadakchap <aliceprerna@gmail.com>
2023-12-07 21:00:35 +0530

    create GhostMessage

---
+++ src/components/EmptyGhostState/EmptyGhostState.react.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Icon from 'components/Icon/Icon.react';
+import styles from 'components/EmptyGhostState/EmptyGhostState.scss';
+
+const EmptyGhostState = ({ title, description, cta, action }) => {
+  return (
+    <div className={styles.content}>
+      <Icon className={styles.icon} name="ghost-icon" width={32} height={40} />
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>{description}</div>
+      {cta ? (
+        <button className={styles.cta} onClick={action}>{cta}</button>
+      ) : null}
+    </div>
+  )
+}
+
+export default EmptyGhostState;
