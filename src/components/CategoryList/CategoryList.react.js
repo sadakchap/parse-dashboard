3a5cf11eba4240c43178986b6a1fe8ddc7c0d10a
sadakchap <aliceprerna@gmail.com>
2023-12-07 16:38:35 +0530

    fix categorylist class highlight css

--- src/components/CategoryList/CategoryList.react.js
+++ src/components/CategoryList/CategoryList.react.js
@@ -56,7 +56,7 @@ export default class CategoryList extends React.Component {
               for (let i = 0; i < c.filters?.length; i++) {
                 const filter = c.filters[i];
                 if (queryFilter === filter.filter) {
-                  height += (i + 1) * (20 + 24); // added 24 for margin top & bottom
+                  height += (i + 1) * (20 + 12); // added 24 for margin top & bottom
                   break;
                 }
               }
@@ -69,9 +69,9 @@ export default class CategoryList extends React.Component {
         if (id === 'classSeparator') {
           height += 13;
         } else if (this.state.openClasses.includes(id)) {
-          height = (height + 20 + 24) * (c.filters.length + 1);
+          height = (height + 20 + 12) * (c.filters.length + 1);
         } else {
-          height += 20 + 24;
+          height += 20 + 12;
         }
       }
       this.highlight.style.display = 'none';
