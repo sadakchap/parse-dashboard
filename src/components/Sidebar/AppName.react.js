02846bc060906a66f6a1b0ef7de99f1b44ee0507
sadakchap <aliceprerna@gmail.com>
2024-02-07 15:42:23 +0530

    fix sidebar pins

--- src/components/Sidebar/AppName.react.js
+++ src/components/Sidebar/AppName.react.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Pin from 'components/Sidebar/Pin.react';
 import Tooltip from 'components/Tooltip/SimpleTooltip.react';
+import Icon from 'components/Icon/Icon.react';
 import styles from 'components/Sidebar/B4aSidebar.scss';
 
-const AppName = ({ name, onClick, pin, onPinClick }) => (
+const AppName = ({ name, onClick, onPinClick }) => (
   <div>
     <div className={styles.currentApp}>
       <div className={styles.appNameContainer} onClick={onClick}>
         <Tooltip className={styles.Tooltip} value={(<span>{name}</span>)}>
           <div className={styles.currentAppName}>{name}</div>
         </Tooltip>
-        <div className={styles.appsSelectorArrow}></div>
+        <Icon name="b4a-chevron-down" width={18} height={18} />
       </div>
-      {pin}
+      <Pin onClick={onPinClick} />
     </div>
   </div>
 );
